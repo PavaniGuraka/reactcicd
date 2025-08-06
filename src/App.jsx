c54c0f6a3d8c872b71ffedc468df050eb706b661
PavaniGuraka <2300032383@kluniversity.in>
2025-08-06 10:02:54 +0530

    Added version 3 heading

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Hi this is GIT ACTION Experiment</h1>
+      <h1>version 2 of my project</h1>
+      <h1>version 3 of my project</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
